dbfb147ace092acb377535e88558eb063e35915b
Gurbachan27 <gurbachanparmarbinjon@gmail.com>
2025-05-30 16:30:48 -0400

    added code to script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// js code
+const words = {
+    noun: ['The turkey', 'The monkey', 'My teacher', 'The dog'],
+    verb: ['sat on', 'danced with', 'saw', 'ate'],
+    adjective: ['a funny', 'a scary', 'a slimy', 'a goofy'],
+    object: ['fish', 'bug', 'chair', 'ball'],
+    place: ['in the park', 'at school', 'on the chair', 'in the soup']
+};
+
+let selectedWords = {
+    noun: '',
+    verb: '',
+    adjective: '',
+    object: '',
+    place: ''
+};
